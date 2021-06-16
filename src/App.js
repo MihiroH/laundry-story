0d486f968f48f2ffd9718b2f41e1f1de976e222b
MihiroH <mihiro.hashimoto.1125@gmail.com>
2021-06-16 21:48:26 +0900

    Add: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
+function Index() {
+  return <h2>Home</h2>
+}
+
+function Product({ match }) {
+  return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route
+          path="/"
+          exact
+          component={Index}
+        />
+        <Route
+          path="/products/:id"
+          component={Product}
+        />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
